28034f9a5a4515db85792798da07754878d282ce
rookie <675611058@qq.com>
2023-09-11 10:05:34 +0800

    main branch: modify common project to maven project

---
+++ src/main/java/com/rookie/App.java
@@ -0,0 +1,13 @@
+package com.rookie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
